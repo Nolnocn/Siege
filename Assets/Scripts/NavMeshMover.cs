6fd91a687db2f1764a0fe81cd3d99d48f9ae806e
ckaron0912 <ctk5520@g.rit.edu>
2016-02-24 21:46:25 -0500

    -Added NavMesh controller to move characters -Added a script to control the walls being destroyed

---
+++ Assets/Scripts/NavMeshMover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshMover : MonoBehaviour 
+{
+    // simple point and click navmesh mover script
+	private NavMeshAgent m_agent;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+        m_agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+        if( Input.GetMouseButtonDown(0) )
+        {
+            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+            RaycastHit hit;
+
+            if( Physics.Raycast( ray, out hit, 100 ) )
+            {
+                m_agent.SetDestination( hit.point );
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/WallController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallController : MonoBehaviour 
+{
+    public int health = 100;
+    private bool m_destroyed = false;
+    public GameObject m_wall_destroyed;
+
+	// Use this for initialization
+	void Start() 
+    {
+	}
+	
+	// Update is called once per frame
+	void Update() 
+    {
+        checkHealth();
+
+        if( m_destroyed )
+        {
+            m_wall_destroyed.SetActive( true );
+            gameObject.SetActive( false );
+        }
+	}
+
+    public void TakeDamage( int damage )
+    {
+        health -= damage;
+    }
+
+    private void checkHealth()
+    {
+        if( health <= 0 )
+        {
+            health = 0;
+            m_destroyed = true;
+        }
+    }
+}
