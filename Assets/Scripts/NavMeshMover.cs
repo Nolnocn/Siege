ef85bef19c81bba4246117a12be8b31530093512
Nick Conlon <ncon67@gmail.com>
2016-02-25 13:58:02 -0500

    Added a marker to show where the navmeshmover's destination

--- Assets/Scripts/NavMeshMover.cs
+++ Assets/Scripts/NavMeshMover.cs
@@ -6,26 +6,32 @@ public class NavMeshMover : MonoBehaviour
     // simple point and click navmesh mover script
 	private NavMeshAgent m_agent;
 
+	private GameObject m_desitinationMarker;
+
 	// Use this for initialization
 	void Start () 
     {
-	
-        m_agent = GetComponent<NavMeshAgent>();
+		m_desitinationMarker = GameObject.CreatePrimitive( PrimitiveType.Sphere );
+		m_desitinationMarker.SetActive( false );
+		m_desitinationMarker.transform.localScale = Vector3.one * 0.5f;
+		m_desitinationMarker.GetComponent<Collider>().enabled = false;
+		m_agent = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	
-        if( Input.GetMouseButtonDown( 1 ) )
-        {
-            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
-            RaycastHit hit;
+		if( Input.GetMouseButtonDown( 1 ) )
+		{
+			Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+			RaycastHit hit;
 
-            if( Physics.Raycast( ray, out hit, 100 ) )
-            {
-                m_agent.SetDestination( hit.point );
-            }
-        }
+			if( Physics.Raycast( ray, out hit, 100 ) )
+			{
+				m_agent.SetDestination( hit.point );
+				m_desitinationMarker.transform.position = hit.point;
+				m_desitinationMarker.SetActive( true );
+			}
+		}
 	}
 }
