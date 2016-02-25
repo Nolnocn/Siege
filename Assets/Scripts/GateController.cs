aff62ebe00532ea5b4cc658e1e45e533bb21e034
Nick Conlon <ncon67@gmail.com>
2016-02-25 13:59:00 -0500

    Added a simple gate control to toggle a wall on and off

---
+++ Assets/Scripts/GateController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateController : MonoBehaviour
+{
+	public GameObject wall;
+	public GameObject destroyedObstacle;
+
+	void Update()
+	{
+		if( Input.GetKeyDown( KeyCode.Space ) )
+		{
+			wall.SetActive( !wall.activeInHierarchy );
+			destroyedObstacle.SetActive( !destroyedObstacle.activeInHierarchy );
+		}
+	}
+}
