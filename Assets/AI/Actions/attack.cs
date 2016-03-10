ff6f11e6f1f24fe62d423ad53046a82fdb6a15e5
Zachery Shuffield (RIT Student) <zjs1316@ad.rit.edu>
2016-03-10 16:42:00 -0500

    added attack and hp scripts, behavior tree updated but broken.

---
+++ Assets/AI/Actions/attack.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+
+
+[RAINAction]
+public class attack : RAINAction
+{
+	GameObject detected;
+	float attackCooldown = 2f;
+	float attackTimer = 0f;
+	Health targetHealthscript;
+    public override void Start(RAIN.Core.AI ai)
+    {
+        base.Start(ai);
+    }
+
+    public override ActionResult Execute(RAIN.Core.AI ai)
+    {
+		if(attackTimer + attackCooldown<Time.time)
+		{
+			//GameObject detected = agent.WorkingMemory.GetItem.<GameObject>(“defenderAspect”);
+			GameObject detected = ai.WorkingMemory.GetItem ("enemyPosition") as GameObject;
+			targetHealthscript = detected.GetComponent<Health> ();
+			targetHealthscript.hp -= 10;
+			Debug.Log("Enemy hit, current hp is " + targetHealthscript.hp);
+			attackTimer=Time.time;
+		}
+		return ActionResult.SUCCESS;
+    }
+
+    public override void Stop(RAIN.Core.AI ai)
+    {
+        base.Stop(ai);
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public float hp = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hp <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
